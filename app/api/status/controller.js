cd723f7de708ba9c6125be70f4e7c09bbb678090
markoka101 <markoka101@gmail.com>
2025-04-30 18:56:27 -0400

    only send 200 if ready state is 1

--- app/api/status/controller.js
+++ app/api/status/controller.js
@@ -1,5 +1,13 @@
-exports.currentStatus = function (req, res) {
-  res.status(200).send({
-    status: 'OK'
-  })
+const MongoDB = require('app/lib/mongodb.js')
+
+exports.currentStatus = async function (req, res) {
+  if (MongoDB.readyState === 1) {
+    res.status(200).send({
+      status: 'OK'
+    })
+  } else {
+    res.status(500).send({
+      status: 'INTERNAL SERVER ERROR'
+    })
+  }
 }
