e4b61526354b89a4f0685ab62c5f100588cf56b1
markoka101 <markoka101@gmail.com>
2025-05-01 13:27:27 -0400

    implement controller file

---
+++ app/api/note/controller.js
@@ -0,0 +1,17 @@
+const noteService = require('app/modules/notes')
+
+/**
+ * @method create
+ */
+exports.create = async (req, res) => {
+  const note = await noteService.createForUser(req.body)
+  res.status(201).send(note)
+}
+
+/**
+ * @method list
+ */
+exports.list = async (req, res) => {
+  const notes = await noteService.findByUser(req.params.id)
+  res.status(200).send(notes)
+}
