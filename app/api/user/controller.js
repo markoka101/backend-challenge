cff8e764cec3c8e7ee2464338bfd91695c1ada9b
markoka101 <markoka101@gmail.com>
2025-05-01 13:36:53 -0400

    moved function to get notes to the router in the note directory

--- app/api/user/controller.js
+++ app/api/user/controller.js
@@ -1,4 +1,5 @@
 const userService = require('app/modules/user')
+const noteService = require('app/modules/notes')
 
 /**
  * @method read
@@ -12,6 +13,6 @@ exports.read = async (req, res) => {
  * @method update
  */
 exports.update = async (req, res) => {
-  const user = await userService.readAndUpdate(req.params.id, req.body)
+  const user = await userService.findByIdAndUpdate(req.params.id, req.body)
   res.status(200).send(user)
 }
